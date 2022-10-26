3b256db0d96acc616581c1875bb06322554c7ba7
HB <hbyusuf40@gmail.com>
2022-10-26 20:37:13 +0100

    Implement score and update game status
    fixed limit of 20(twenty) is added and with every attempt made the score get reduced by 1(one) until the score gets to 0(zero). And with every attempt made the guess(inputed value) is compared with the secret value(randomly generated number between 1 and 20) and the game status is updated with either "📉 Too low!", "📈 Too high!" or "🎉 Correct number"

--- script.js
+++ script.js
@@ -2,18 +2,33 @@
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
+let score = document.querySelector('.score').textContent;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  // console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ Insert a number';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = '📈 Too high!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🔥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = '📉 Too low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🔥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
