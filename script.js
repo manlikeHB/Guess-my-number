214dda62f368dc640b34493fd7776e1c47cc31fc
HB <hbyusuf40@gmail.com>
2022-10-26 20:13:10 +0100

    create Guess my number game

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ Insert a number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct number';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+  }
+});
